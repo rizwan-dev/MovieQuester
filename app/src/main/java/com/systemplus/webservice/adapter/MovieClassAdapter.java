93e9864be8ef9eaeb1441079302f2568bece81bb
Rizwanul Haque <rizwan@relfor.com>
2018-02-10 11:25:29 +0530

    Added adapter class, displayed data from webservice to recylerview

---
+++ app/src/main/java/com/systemplus/webservice/adapter/MovieClassAdapter.java
@@ -0,0 +1,61 @@
+package com.systemplus.webservice.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.systemplus.webservice.R;
+import com.systemplus.webservice.model.Result;
+
+import java.util.List;
+
+/**
+ * Created by rizwan on 1/11/17.
+ */
+
+public class MovieClassAdapter extends RecyclerView.Adapter<MovieClassAdapter.MovieHolder> {
+
+    private List<Result> results;
+    private Context context;
+
+    public MovieClassAdapter(List<Result> results, Context context) {
+        this.results = results;
+        this.context = context;
+    }
+
+    @Override
+    public MovieHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_item, parent, false);
+        return new MovieHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieHolder holder, int position) {
+        Result result = results.get(position);
+        holder.txtTitle.setText("Title : "+result.getTitle());
+        holder.txtOverView.setText("Overview : "+result.getOverview());
+        holder.txtRating.setText("Vote Count : "+result.getVoteCount());
+        holder.txtVote.setText("Rating : "+result.getVoteAverage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return results.size();
+    }
+
+    class MovieHolder extends RecyclerView.ViewHolder {
+        TextView txtTitle, txtOverView, txtRating, txtVote;
+
+        public MovieHolder(View itemView) {
+            super(itemView);
+            txtTitle = (TextView)itemView.findViewById(R.id.txtTitle);
+            txtOverView = (TextView)itemView.findViewById(R.id.txtOverView);
+            txtRating = (TextView)itemView.findViewById(R.id.txtRating);
+            txtVote = (TextView)itemView.findViewById(R.id.txtVote);
+
+        }
+    }
+}
--- app/src/main/java/com/systemplus/webservice/view/MainActivity.java
+++ app/src/main/java/com/systemplus/webservice/view/MainActivity.java
@@ -1,11 +1,16 @@
 package com.systemplus.webservice.view;
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.systemplus.webservice.R;
+import com.systemplus.webservice.adapter.MovieClassAdapter;
 import com.systemplus.webservice.api.ApiClient;
 import com.systemplus.webservice.api.ApiInterface;
 import com.systemplus.webservice.model.MovieData;
@@ -19,7 +24,10 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView moviesList;
+
     private static final String API_KEY = "7e8f60e325cd06e164799af1e317d7a7";
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +36,47 @@ public class MainActivity extends AppCompatActivity {
         final ApiInterface apiService =
                 ApiClient.getClient().create(ApiInterface.class);
 
+        moviesList = findViewById(R.id.moviesList);
+
         Call<MovieData> call = apiService.getTopRatedMovies(API_KEY);
 
+        showProgressDialog();
         call.enqueue(new Callback<MovieData>() {
             @Override
             public void onResponse(Call<MovieData> call, Response<MovieData> response) {
+                hidProgressDialog();
                 MovieData movieData = response.body();
                 List<Result> moviesResult = movieData.getResults();
-                showToast(new Gson().toJson(movieData).toString());
+
+                MovieClassAdapter movieClassAdapter = new MovieClassAdapter(moviesResult, MainActivity.this);
+                moviesList.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                moviesList.setAdapter(movieClassAdapter);
 
             }
 
             @Override
             public void onFailure(Call<MovieData> call, Throwable t) {
+                hidProgressDialog();
                 showToast(t.getMessage());
             }
         });
     }
+
+    private void hidProgressDialog() {
+        if(mProgressDialog!=null && mProgressDialog.isShowing()){
+            mProgressDialog.dismiss();
+        }
+
+    }
+
+    private void showProgressDialog() {
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setMessage("Loading data, Please wait");
+        mProgressDialog.setCancelable(false);
+        mProgressDialog.show();
+    }
+
+
 
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
