cdc358b4e200a34a5462de79a7c13e0914759473
Rizwanul Haque <rizwan@relfor.com>
2018-02-18 10:51:34 +0530

    Added code for drawing polyline

---
+++ app/src/main/java/com/systemplus/webservice/view/MapDemoActivity.java
@@ -0,0 +1,73 @@
+package com.systemplus.webservice.view;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.systemplus.webservice.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapDemoActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    private static final LatLng DRIVER_LOCATION = new LatLng(18.540556, 73.904048);
+
+    private static final LatLng CURBEE_LOCATION = new LatLng(18.543440, 73.905482   );
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_demo);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        builder.include(DRIVER_LOCATION);
+        builder.include(CURBEE_LOCATION);
+        LatLngBounds bounds = builder.build();
+
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 200);
+
+        mMap.animateCamera(cu);
+
+        PolylineOptions polylineOptions = new PolylineOptions().width(10).geodesic(true).color(Color.CYAN);
+        List<LatLng> points =   new ArrayList<>();
+        points.add(CURBEE_LOCATION);
+        points.add(DRIVER_LOCATION);
+
+        Polyline polyline = mMap.addPolyline(polylineOptions);
+    }
+
+}
